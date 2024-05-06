505a378a48b67b2c6ffdbe6714d07539b929dce6
Gabriel Bello <gabrielassis.6747@aluno.saojudas.br>
2024-05-06 18:40:58 -0300

    Classe Record criada

---
+++ Challenge-Conversor-de-Moedas/src/Dados.java
@@ -0,0 +1,4 @@
+public record Dados (String BRL, String USD, String EUR, String CAD,
+                     String ARS, String GBP,String COP, String GHS,
+                     String EGP, String AUD, String KRW, String FJD) {
+}
